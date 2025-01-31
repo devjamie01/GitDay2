408d77a9e94adcf610eaa48b33e96432f28d66eb
devjamie <dev.j.pewu@gmail.com>
2025-01-31 18:48:30 -0500

    statch

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -15,4 +15,18 @@ public class DemoDay2 {
 		//this code is coming from github
 
 }
-}
+	
+	
+	public void add (int a, int b) {
+		
+		System.out.println(a+b);
+	}
+		
+		
+		public void subtract (int d, int t) {
+			
+		
+			System.out.println(d-t);
+		}
+	}
+
